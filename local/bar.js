37f9f6b8af17aeed7b3ff9e2336853de25d9e225
andy870622 <35159550+andy870622@users.noreply.github.com>
2019-03-19 12:08:24 +0800

    改

--- local/bar.js
+++ local/bar.js
@@ -1,8 +1,9 @@
+var BackString = "<canvas style='z-index: -1;position: absolute; left:-1%;top:-1%;' id='canvas'></canvas><link rel='stylesheet' type='text/css'href='../local/OSXcss.css'><script src = '../local/back.js'></script> " // 背景
 var StartString = "<div id='dockContainer'><div id='dockWrapper'><div class='cap left'></div><ul class='osx-dock'>"
-var HomeString = "<li class='active'><span>Home</span><a href='index.html'><img src='resources/home.png'></a></li>"
-var HelpString = "<li class='active'><span>Help</span><a><img src='resources/help.png'></a></li>"
-var EditString = "<li class='active'><span>Edit</span><a><img src='resources/notebook.png'></a></li>"
+var HomeString = "<li class='active'><span>Home</span><a href='../web/index.html'><img src='../resources/home.png'></a></li>"
+var HelpString = "<li class='active'><span>Help</span><a ><img src='../resources/help.png'></a></li>"
+var EditString = "<li class='active'><span>Edit</span><a href= '../web/MidiEdit.html'><img src='../resources/notebook.png'></a></li>"
 var EndString = "</ul></div></div>"
 
-String = StartString + HomeString + HelpString + EditString + EndString;
-document.write(String);
+String = BackString + StartString + HomeString + HelpString + EditString + EndString;
+document.write(String)
