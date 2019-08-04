e20317347d83a8e94a40c14427d0a82ca2e4d87f
andy870622 <35159550+andy870622@users.noreply.github.com>
2019-08-05 01:32:37 +0800

    Update bar.js

--- js/bar.js
+++ js/bar.js
@@ -1,4 +1,4 @@
-var BackString = "<canvas style='z-index: -1;float:left;background-attachment:fixed; position:fixed;' id='canvas'></canvas><link rel='stylesheet' type='text/css'href='../css/OSXcss.css'> " // 背景  <script src = '../js/back.js'></script>
+var BackString = "<canvas style='z-index: -1;float:left;background-attachment:fixed; position:fixed;' id='canvas'></canvas><script src = '../js/back.js'></script><link rel='stylesheet' type='text/css'href='../css/OSXcss.css'> " // 背景  <script src = '../js/back.js'></script>
 var StartString = "<div id='dockContainer'><div id='dockWrapper'><div class='cap left'></div><ul class='osx-dock'>"
 var HomeString = "<li class='active'><span>Home</span><a href='../web/index.html'><img src='../resources/home.png'></a></li>"
 var HelpString = "<li class='active'><span>Help</span><a href='../web/help.html' ><img src='../resources/help.png'></a></li>"
